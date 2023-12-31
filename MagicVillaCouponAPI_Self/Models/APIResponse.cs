d4b0ede31923f0e91c96ea5544e41fb79f5a7154
mreshboboyev <mreshboboyev@gmail.com>
2023-12-31 17:31:56 +0500

    Fields added in APIResponse.cs : IsSuccess, Result, StatusCode, ErrorMessages; Ctor: ErrorMessages = new()

--- MagicVillaCouponAPI_Self/Models/APIResponse.cs
+++ MagicVillaCouponAPI_Self/Models/APIResponse.cs
@@ -1,6 +1,16 @@
-﻿namespace MagicVillaCouponAPI_Self.Models
+﻿using System.Net;
+
+namespace MagicVillaCouponAPI_Self.Models
 {
     public class APIResponse
     {
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+        public bool IsSuccess { get; set; }
+        public Object Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
     }
 }
