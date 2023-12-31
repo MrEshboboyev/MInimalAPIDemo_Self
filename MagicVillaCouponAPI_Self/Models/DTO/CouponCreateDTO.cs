bdccc0715fa946a1083866954724814842d0acc0
mreshboboyev <mreshboboyev@gmail.com>
2023-12-31 15:03:27 +0500

    CouponCreateDTO : Name, Percent, IsActive; CouponDTO : Id, Name, Percent, IsActive, Created

--- MagicVillaCouponAPI_Self/Models/DTO/CouponCreateDTO.cs
+++ MagicVillaCouponAPI_Self/Models/DTO/CouponCreateDTO.cs
@@ -2,5 +2,8 @@
 {
     public class CouponCreateDTO
     {
+        public string Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
     }
 }
--- MagicVillaCouponAPI_Self/Models/DTO/CouponDTO.cs
+++ MagicVillaCouponAPI_Self/Models/DTO/CouponDTO.cs
@@ -2,5 +2,10 @@
 {
     public class CouponDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Created { get; set; }
     }
 }
