30680307e6a6955d43d280359890828243a1b469
mreshboboyev <mreshboboyev@gmail.com>
2023-12-31 18:10:49 +0500

    Creating CouponUpdateValidation.cs: 'Id', 'Name', 'Percent'; ID is greater than zero; Name is not empty; Percent in Range[1, 100]

---
+++ MagicVillaCouponAPI_Self/Validations/CouponUpdateValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MagicVillaCouponAPI_Self.Models.DTO;
+
+namespace MagicVillaCouponAPI_Self.Validations
+{
+    public class CouponUpdateValidation : AbstractValidator<CouponUpdateDTO>
+    {
+        public CouponUpdateValidation() 
+        {
+            RuleFor(coupon => coupon.Id).NotEmpty().GreaterThan(0);
+            RuleFor(coupon => coupon.Name).NotEmpty();
+            RuleFor(coupon => coupon.Percent).InclusiveBetween(1, 100);
+        }
+    }
+}
