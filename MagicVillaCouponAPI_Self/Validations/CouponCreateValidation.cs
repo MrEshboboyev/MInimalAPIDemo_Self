1709279fdda5930604cb6fe75d19433428a3cb60
mreshboboyev <mreshboboyev@gmail.com>
2023-12-31 17:13:46 +0500

    Ruling for fields in CouponCreateValidation.cs : 'Name', 'Percent'; Name is not empty; Percent in Range[1, 100]

--- MagicVillaCouponAPI_Self/Validations/CouponCreateValidation.cs
+++ MagicVillaCouponAPI_Self/Validations/CouponCreateValidation.cs
@@ -1,9 +1,15 @@
 ﻿using FluentValidation;
 using MagicVillaCouponAPI_Self.Models.DTO;
+using System.Data;
 
 namespace MagicVillaCouponAPI_Self.Validations
 {
     public class CouponCreateValidation : AbstractValidator<CouponCreateDTO>
     {
+        public CouponCreateValidation() 
+        {
+            RuleFor(coupon => coupon.Name).NotEmpty();
+            RuleFor(coupon => coupon.Percent).InclusiveBetween(1, 100);
+        }
     }
 }
